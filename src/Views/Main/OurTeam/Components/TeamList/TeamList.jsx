6d4448a92a8266ada1ece5ac1e9eeefa4f70db44
Marco Giubellino <marco.giubellino@jcmanagement.com>
2023-01-30 17:00:08 +0100

    TeanList + assets

--- src/Views/Main/OurTeam/Components/TeamList/TeamList.jsx
+++ src/Views/Main/OurTeam/Components/TeamList/TeamList.jsx
@@ -1,7 +1,55 @@
+//Libs
+import { Container } from "@hybris-software/ui-kit"
+
+//Assets
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa'
+
+//Data
+import { teamMembers } from './data.js'
+
+//Style
+import Style from './TeamList.module.css'
+
+//Constants
+const socialsIcons = {
+    facebook: <FaFacebookF size={20} />,
+    twitter: <FaTwitter size={20} />,
+    instagram: <FaInstagram size={20} />,
+    linkedin: <FaLinkedinIn size={20} />,
+}
+
 const TeamList = () => {
     return (
-        <div>
+        <Container className={Style.componentWrapper}>
+            {
+                teamMembers.map((data, index) => {
+                    return <TeamMember {...data} key={index} />
+                })
+            }
+        </Container>
+    )
+}
 
+const TeamMember = ({ fullname, caption, image, socials }) => {
+    return (
+        <div className={Style.teamMember}>
+            <div className={Style.memberImage}>
+                <img src={image} alt={fullname} draggable={false} />
+            </div>
+            <div className={Style.memberContent}>
+                <h6>{fullname}</h6>
+                <p>{caption}</p>
+                <div className={Style.socials}>
+                    {
+                        Object.keys(socials).map((key, index) => {
+                            if (socialsIcons[key] !== undefined)
+                                return (
+                                    <a href={socials[key]} target={"_blank"} rel="noreferrer" key={index}>{socialsIcons[key]}</a>
+                                )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/Views/Main/OurTeam/Components/TeamList/data.js
@@ -0,0 +1,83 @@
+//Assets
+import AnilSethi from '../../../../../Assets/images/cropped_team/anil-sethi.png'
+import AndreaNardon from '../../../../../Assets/images/cropped_team/andrea-nardon.png'
+import FlavioVilla from '../../../../../Assets/images/cropped_team/flavio-villa.png'
+import DanieleSinacori from '../../../../../Assets/images/cropped_team/daniele sinacori.png'
+import GiuseppeSinacori from '../../../../../Assets/images/cropped_team/giuseppe sinacori.png'
+import RamonVega from '../../../../../Assets/images/cropped_team/ramon-vega.png'
+import AlessioVinassa from '../../../../../Assets/images/cropped_team/alessio vinassa.png'
+
+const teamMembers = [
+    {
+        fullname: "Anil Sethi",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: AnilSethi,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+    {
+        fullname: "Andrea Nardon",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: AndreaNardon,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+    {
+        fullname: "Flavio Villa",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: FlavioVilla,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+    {
+        fullname: "Daniele Sinacori",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: DanieleSinacori,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+    {
+        fullname: "Giuseppe Sinacori",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: GiuseppeSinacori,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+    {
+        fullname: "Ramon Vega",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: RamonVega,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+    {
+        fullname: "Alessio Vinassa",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: AlessioVinassa,
+        socials: {
+            facebook: "",
+            twitter: "",
+            instagram: ""
+        }
+    },
+]
+
+export { teamMembers }
