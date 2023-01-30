e6e32f7af0e47c8bc9a236b274cf58892d391359
Marco Giubellino <marco.giubellino@jcmanagement.com>
2023-01-30 17:52:35 +0100

    Newsletter Banner

--- src/Views/Main/OurTeam/Components/NewsletterBanner/NewsletterBanner.jsx
+++ src/Views/Main/OurTeam/Components/NewsletterBanner/NewsletterBanner.jsx
@@ -1,8 +1,20 @@
+//Libs
+import { Button, Container } from '@hybris-software/ui-kit'
+
+//Style
+import Style from './NewsletterBanner.module.css'
+
 const NewsletterBanner = () => {
     return (
-        <div>
-
-        </div>
+        <Container className={Style.componentWrapper}>
+            <div className={Style.banner}>
+                <div className={Style.text}>
+                    <h4>Get in contact</h4>
+                    <p>Want to know more? Just reach out to us.</p>
+                </div>
+                <Button buttonClassName={Style.button}>SUBSCRIBE TO OUR NEWSLETTER</Button>
+            </div>
+        </Container>
     )
 }
 
