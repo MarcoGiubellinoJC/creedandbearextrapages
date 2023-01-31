66039b6165f7273933e7b9bff9dd37d6d59fa4e9
Marco Giubellino <marco.giubellino@jcmanagement.com>
2023-01-31 10:46:10 +0100

    OurTeam HeroCaption

--- src/Views/Main/OurTeam/Components/HeroSection/HeroSection.jsx
+++ src/Views/Main/OurTeam/Components/HeroSection/HeroSection.jsx
@@ -10,18 +10,13 @@ import Style from './HeroSection.module.css'
 
 const HeroSection = () => {
     return (
-        <>
-            <Container className={Style.componentWrapper}>
-                <div className={Style.pattern} />
-                <div className={Style.content}>
-                    <h1 className={Style.title}>OUR SKILLED TEAM</h1>
-                    <img className={Style.heroImage} src={HeroImage} alt="heroimage" draggable={false} />
-                </div>
-            </Container>
-            <Container>
-                <p className={Style.caption}>In today’s complex and global marketplace, all sell side entities, from the largest investment banks to the small and independent brokers, are facing fierce competition.</p>
-            </Container>
-        </>
+        <Container className={Style.componentWrapper}>
+            <div className={Style.pattern} />
+            <div className={Style.content}>
+                <h1 className={Style.title}>OUR SKILLED TEAM</h1>
+                <img className={Style.heroImage} src={HeroImage} alt="heroimage" draggable={false} />
+            </div>
+        </Container>
     )
 }
 
--- src/Views/Main/OurTeam/OurTeam.jsx
+++ src/Views/Main/OurTeam/OurTeam.jsx
@@ -1,3 +1,6 @@
+//Libs
+import { Container } from "@hybris-software/ui-kit"
+
 //Components
 import HeroSection from "./Components/HeroSection/HeroSection.jsx"
 import LocationBanner from "./Components/LocationBanner/LocationBanner.jsx"
@@ -11,6 +14,9 @@ const OurTeam = () => {
     return (
         <div className={Style.pageWrapper}>
             <HeroSection />
+            <Container>
+                <p className={Style.caption}>In today’s complex and global marketplace, all sell side entities, from the largest investment banks to the small and independent brokers, are facing fierce competition.</p>
+            </Container>
             <LocationBanner />
             <TeamList />
             <NewsletterBanner />
